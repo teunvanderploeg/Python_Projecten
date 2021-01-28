b5e4d765747af0b8076f503162af7b224d6bf48e
Teun van der Ploeg <29253@ma-web.nl>
2021-01-28 16:38:55 +0100

    Moving Files
    automatiseren van het verplaatsen en hernoemen van files

---
+++ Automaticlly Moving Files/README.md
@@ -0,0 +1,3 @@
+## Automaticlly Moving Files
+
+Ik heb geleerd van dit project dat veel dingen kan automatiseren met python en ik weet nu hoe je dat met het verplaatsen van files moet doen. 
---
+++ Automaticlly Moving Files/moving.py
@@ -0,0 +1,43 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+# pip install watchdog
+
+import os
+import json
+import time
+
+
+class MyHandler(FileSystemEventHandler):
+
+    def on_modified(self, event):
+
+        for filename in os.listdir(folder_to_track):
+
+            src = folder_to_track + "/" + filename
+
+            old_file_name = os.path.basename(src)
+
+            new_name = date + "__" + old_file_name
+
+            # file_exists = os.path.isfile(folder_destination + "/" + new_name)
+
+            new_destination = folder_destination + "/" + new_name
+
+            os.rename(src, new_destination)
+
+
+folder_to_track = "/Users/teun/Bureaublad/testFolder1"
+folder_destination = "/Users/teun/Bureaublad/testFolder2"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(5)
+        date = time.strftime('%Y-%m-%d-%S', time.localtime())
+        print("Try on: " + date)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
